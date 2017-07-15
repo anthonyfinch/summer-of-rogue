1c014cf54749deb8b0fbb530112fcffbcce7806a
Anthony Finch <actionspeakslouder@gmail.com>
2017-07-15 00:39:44 -0700

    Adds basic game state and player rendering

--- src/Sor.hs
+++ src/Sor.hs
@@ -2,11 +2,55 @@ module Sor where
 
 import Graphics.Gloss.Interface.IO.Game
 
+
+data GridCoord = GridCoord
+  {
+    x :: Int,
+    y :: Int
+  }
+
+data Player =  Player
+  {
+    position :: GridCoord
+  }
+
+data GameState = GameState
+  {
+    player :: Player
+  }
+
+
+gameWidth = 800
+gameHeight = 600
+tileSize = 10
+
+initialGameState = GameState
+  {
+    player = Player $ GridCoord 0 0
+  }
+
+gridToPos :: Int -> Float
+gridToPos i = fromIntegral i * tileSize
+
+render :: GameState -> IO Picture
+render state = return (Translate px py $ Color white $ circleSolid tileSize)
+  where
+    pos = position $ player state
+    px = gridToPos $ x $ pos
+    py = gridToPos $ y $ pos
+
+handleEvent :: Event -> GameState -> IO GameState
+handleEvent event state = return state
+
+update :: Float -> GameState -> IO GameState
+update dt state = return state
+
+playSor :: IO ()
 playSor = playIO
-  (InWindow "Summer Of Rogue" (700, 100) (50, 50))
-  white
-  100
-  ""
-  (\str -> return (Translate (-340) 0 $ Scale 0.1 0.1 $ Text str))
-  (\event _ -> return (show event))
-  (\_ world -> return world)
+  (InWindow "Summer Of Rogue" (gameWidth, gameHeight) (50, 50))
+  black
+  60
+  initialGameState
+  render
+  handleEvent
+  update
